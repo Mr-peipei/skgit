fc512ed93344ca7c23bde0f20fd139215c9f5d43
Mr-peipei <wushumenmenmen2@gmail.com>
2022-11-28 22:23:41 +0900

    refactor: 💡 delete warnings

--- src/common/command.rs
+++ src/common/command.rs
@@ -1,9 +1,9 @@
-use std::process::{Command, Output};
+// use std::process::{Command, Output};
 
-pub fn execute_command(com: &str, item: String) -> Output {
-    let output = Command::new(&com)
-        .arg(item)
-        .output()
-        .expect("something went wrong");
-    output
-}
+// pub fn execute_command(com: &str, item: String) -> Output {
+//     let output = Command::new(&com)
+//         .arg(item)
+//         .output()
+//         .expect("something went wrong");
+//     output
+// }
--- src/common/tasks.rs
+++ src/common/tasks.rs
@@ -1,96 +1,96 @@
-use chrono::{serde::ts_seconds, DateTime, Local, Utc};
-use core::fmt;
-use serde::Deserialize;
-use serde::Serialize;
-use std::fs::{File, OpenOptions};
-use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
-use std::path::PathBuf;
+// use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+// use core::fmt;
+// use serde::Deserialize;
+// use serde::Serialize;
+// use std::fs::{File, OpenOptions};
+// use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
+// use std::path::PathBuf;
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Task {
-    pub text: String,
+// #[derive(Debug, Deserialize, Serialize)]
+// pub struct Task {
+//     pub text: String,
 
-    #[serde(with = "ts_seconds")]
-    pub created_at: DateTime<Utc>,
-}
+//     #[serde(with = "ts_seconds")]
+//     pub created_at: DateTime<Utc>,
+// }
 
-impl Task {
-    pub fn new(text: String) -> Task {
-        let created_at: DateTime<Utc> = Utc::now();
-        Task { text, created_at }
-    }
-}
+// impl Task {
+//     pub fn new(text: String) -> Task {
+//         let created_at: DateTime<Utc> = Utc::now();
+//         Task { text, created_at }
+//     }
+// }
 
-pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
-    // Open the file.
-    let file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(journal_path)?;
-    let mut tasks = collect_tasks(&file)?;
-    tasks.push(task);
-    serde_json::to_writer(file, &tasks)?;
+// pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+//     // Open the file.
+//     let file = OpenOptions::new()
+//         .read(true)
+//         .write(true)
+//         .create(true)
+//         .open(journal_path)?;
+//     let mut tasks = collect_tasks(&file)?;
+//     tasks.push(task);
+//     serde_json::to_writer(file, &tasks)?;
 
-    Ok(())
-}
+//     Ok(())
+// }
 
-pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
-    // Open the file.
-    let file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(journal_path)?;
+// pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+//     // Open the file.
+//     let file = OpenOptions::new()
+//         .read(true)
+//         .write(true)
+//         .open(journal_path)?;
 
-    let mut tasks = collect_tasks(&file)?;
-    // Remove the task.
-    if task_position == 0 || task_position > tasks.len() {
-        return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
-    }
-    tasks.remove(task_position - 1);
+//     let mut tasks = collect_tasks(&file)?;
+//     // Remove the task.
+//     if task_position == 0 || task_position > tasks.len() {
+//         return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+//     }
+//     tasks.remove(task_position - 1);
 
-    // Rewind and truncate the file.
-    file.set_len(0)?;
+//     // Rewind and truncate the file.
+//     file.set_len(0)?;
 
-    // Write the modified task list back into the file.
-    serde_json::to_writer(file, &tasks)?;
-    Ok(())
-}
+//     // Write the modified task list back into the file.
+//     serde_json::to_writer(file, &tasks)?;
+//     Ok(())
+// }
 
-pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
-    // Open the file.
-    let file = OpenOptions::new().read(true).open(journal_path)?;
-    // Parse the file and collect the tasks.
-    let tasks = collect_tasks(&file)?;
+// pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+//     // Open the file.
+//     let file = OpenOptions::new().read(true).open(journal_path)?;
+//     // Parse the file and collect the tasks.
+//     let tasks = collect_tasks(&file)?;
 
-    // Enumerate and display tasks, if any.
-    if tasks.is_empty() {
-        println!("Task list is empty!");
-    } else {
-        let mut order: u32 = 1;
-        for task in tasks {
-            println!("{}: {}", order, task);
-            order += 1;
-        }
-    }
+//     // Enumerate and display tasks, if any.
+//     if tasks.is_empty() {
+//         println!("Task list is empty!");
+//     } else {
+//         let mut order: u32 = 1;
+//         for task in tasks {
+//             println!("{}: {}", order, task);
+//             order += 1;
+//         }
+//     }
 
-    Ok(())
-}
+//     Ok(())
+// }
 
-fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
-    file.seek(SeekFrom::Start(0))?; // Rewind the file before.
-    let tasks = match serde_json::from_reader(file) {
-        Ok(tasks) => tasks,
-        Err(e) if e.is_eof() => Vec::new(),
-        Err(e) => Err(e)?,
-    };
-    file.seek(SeekFrom::Start(0))?; // Rewind the file after.
-    Ok(tasks)
-}
+// fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+//     file.seek(SeekFrom::Start(0))?; // Rewind the file before.
+//     let tasks = match serde_json::from_reader(file) {
+//         Ok(tasks) => tasks,
+//         Err(e) if e.is_eof() => Vec::new(),
+//         Err(e) => Err(e)?,
+//     };
+//     file.seek(SeekFrom::Start(0))?; // Rewind the file after.
+//     Ok(tasks)
+// }
 
-impl fmt::Display for Task {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
-        write!(f, "{:<50} [{}]", self.text, created_at)
-    }
-}
+// impl fmt::Display for Task {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+//         write!(f, "{:<50} [{}]", self.text, created_at)
+//     }
+// }
